92978e02c8a8d32dcda88d91ec40ff0f4bc7bd89
liyunfan1223 <56597220+liyunfan1223@users.noreply.github.com>
2022-10-18 20:47:03 +0800

    Arc2, scripts and visualization (#6)
    * feat: ARC2
    
    * feat: ARC3
    
    * feat: ARC_2 & ARC_3 & params for main
    
    * feat: scripts & visualization
    
    Co-authored-by: Yunfan Li <liyunfan2@huawei.com>

--- README.md
+++ README.md
@@ -11,22 +11,33 @@ cmake .. && make
 
 ## Usage
 ```
-./src/main <buffer_size> <trace_file>
+./src/main <cache_policy> <buffer_size> <trace_file> <params>?
 ```
 
 ## Example
 ```
-./src/main 65536 ../traces/P1.lis
+./src/main -h
+usage: ./src/main <cache_policy> <buffer_size> <trace_file> <param_0>
+       <cache_policy> -- cache policy in {LRU, LFU, ARC, ARC_2, ARC_3, FF}
+       <buffer_size>  -- buffer size
+       <trace_file>   -- path of trace_file
+```
+```
+./src/main LRU 65536 ../traces/P1.lis
 LRU_CACHE_MANAGER:  buffer_size:65536 hit_count:11011495 miss_count:21043978 hit_rate:34.3514%
-LFU_CACHE_MANAGER:  buffer_size:65536 hit_count:12421166 miss_count:19634307 hit_rate:38.749%
-ARC_CACHE_MANAGER:  buffer_size:65536 hit_count:14268223 miss_count:17787250 hit_rate:44.511%
-FF_CACHE_MANAGER:  buffer_size:65536 hit_count:20313683 miss_count:11741790 hit_rate:63.3704%
 ```
 
 ## Unittest & Coverage
 ```
 ctest && make coverage
 ```
+
+## Scripts & Visualization
+```
+./scripts/main.py
+```
+Find plot in local/plot.png:
+![](docs/pics/example_visualization.png)
 ## TODO
 
 ### Cache policies
@@ -34,16 +45,18 @@ ctest && make coverage
 - [x] LFU
 - [x] ARC
 - [x] FF
-- [ ] ARC-2
+- [x] ARC-2
+- [x] ARC-3
 - [ ] hybrid LRU-LFU
 - [ ] ...
 
 ### Features
 - [x] cmake build
 - [x] unittest & code coverage
+- [x] test scripts
+- [x] result visualization
 - [ ] log
 - [ ] more parameters (e.g. scales, policies, unique keys)
-- [ ] test scripts
 - [ ] ...
 
 ### Metrics
--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,67 @@
 #include "managers/lru_cache_manager.h"
 #include "managers/lfu_cache_manager.h"
 #include "managers/arc_cache_manager.h"
+#include "managers/arc2_cache_manager.h"
 #include "managers/ff_cache_manager.h"
+#include "managers/arc3_cache_manager.h"
+
+std::unordered_map<std::string, CachePolicy> cachePolicy = {
+        {"LRU", CachePolicy::LRU},
+        {"LFU", CachePolicy::LFU},
+        {"ARC", CachePolicy::ARC},
+        {"ARC_2", CachePolicy::ARC_2},
+        {"ARC_3", CachePolicy::ARC_3},
+        {"FF", CachePolicy::FF}
+};
+
+void usage() {
+    std::cout << "usage: ./src/main" << " <cache_policy> <buffer_size> <trace_file> <param_0>\n"
+              << "       <cache_policy> -- cache policy: [LRU, LFU, ARC, ARC_2, ARC_3, FF]\n"
+              << "       <buffer_size>  -- buffer size\n"
+              << "       <trace_file>   -- path of trace_file" << std::endl;
+}
 
 int main(int argc, char **argv) {
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+        usage();
+        return 0;
+    }
     std::vector<Key> access_list;
-    UnittestUtils::get_access_list(argv[2], access_list);
+    UnittestUtils::get_access_list(argv[3], access_list);
 
-    UnittestUtils::make_test(argv[2], std::shared_ptr<CacheManager>(new LRUCacheManager(std::stoi(argv[1]))));
-    UnittestUtils::make_test(argv[2], std::shared_ptr<CacheManager>(new LFUCacheManager(std::stoi(argv[1]))));
-    UnittestUtils::make_test(argv[2], std::shared_ptr<CacheManager>(new ARCCacheManager(std::stoi(argv[1]))));
-    UnittestUtils::make_test(argv[2], std::shared_ptr<CacheManager>(new FFCacheManager(std::stoi(argv[1]), access_list)));
+    std::string cache_policy(argv[1]);
+    int32_t buffer_size = std::stoi(argv[2]);
+    char * trace_file = argv[3];
+    char * param_0 =argv[4];
+
+    switch (cachePolicy.at(cache_policy)) {
+        case CachePolicy::LRU:
+            UnittestUtils::make_test(trace_file,std::shared_ptr<CacheManager>(new LRUCacheManager(
+                    buffer_size)));
+            break;
+        case CachePolicy::LFU:
+            UnittestUtils::make_test(trace_file,std::shared_ptr<CacheManager>(new LFUCacheManager(
+                    buffer_size)));
+            break;
+        case CachePolicy::ARC:
+            UnittestUtils::make_test(trace_file,std::shared_ptr<CacheManager>(new ARCCacheManager(
+                    buffer_size)));
+            break;
+        case CachePolicy::ARC_2:
+            UnittestUtils::make_test(trace_file,std::shared_ptr<CacheManager>(new ARC2CacheManager(
+                    buffer_size, std::stof(param_0), access_list)));
+            break;
+        case CachePolicy::ARC_3:
+            UnittestUtils::make_test(trace_file,std::shared_ptr<CacheManager>(new ARC3CacheManager(
+                    buffer_size, std::stoi(param_0), access_list)));
+            break;
+        case CachePolicy::FF:
+            UnittestUtils::make_test(trace_file,std::shared_ptr<CacheManager>(new FFCacheManager(
+                    buffer_size, access_list)));
+            break;
+        default:
+            break;
+    }
     return 0;
 }
 
