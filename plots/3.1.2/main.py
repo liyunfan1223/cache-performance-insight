732bea10c34e4ad7fdfed2519287d966ca77cd2f
Yunfan Li <lyf_liyunfan@sina.com>
2023-04-21 16:35:40 +0800

    f

--- plots/3.1.2/main.py
+++ plots/3.1.2/main.py
@@ -36,7 +36,7 @@ def LRFU_OLTP():
         fig, ax = plt.subplots(figsize=(14, 7))
         ax.set_xlabel('Param (lambda)')
         ax.set_ylabel('Hit Ratio(%)')
-        ax.set_title(trace_name)
+        # ax.set_title(trace_name)
         ax.set_xscale('log')
         trace_file = f'traces/{trace_name}.lis'
         param_list = [math.pow(10, i) for i in np.arange(-6, 0, 0.1)]
@@ -62,7 +62,7 @@ def LRFU_P1():
         fig, ax = plt.subplots(figsize=(14, 7))
         ax.set_xlabel('Param (lambda)')
         ax.set_ylabel('Hit Ratio(%)')
-        ax.set_title(trace_name)
+        # ax.set_title(trace_name)
         ax.set_xscale('log')
         trace_file = f'traces/{trace_name}.lis'
         param_list = [math.pow(10, i) for i in np.arange(-6, 0, 0.1)]
@@ -88,7 +88,7 @@ def EFSW_OLTP():
         fig, ax = plt.subplots(figsize=(14, 7))
         ax.set_xlabel('Param (lambda)')
         ax.set_ylabel('Hit Ratio(%)')
-        ax.set_title(trace_name)
+        # ax.set_title(trace_name)
         ax.set_xscale('log')
         trace_file = f'traces/{trace_name}.lis'
         param_list = [math.pow(10, i) for i in np.arange(-2, 3, 0.1)]
@@ -114,7 +114,7 @@ def EFSW_P1():
         fig, ax = plt.subplots(figsize=(14, 7))
         ax.set_xlabel('Param (lambda)')
         ax.set_ylabel('Hit Ratio(%)')
-        ax.set_title(trace_name)
+        # ax.set_title(trace_name)
         ax.set_xscale('log')
         trace_file = f'traces/{trace_name}.lis'
         param_list = [math.pow(10, i) for i in np.arange(-2, 3, 0.1)]
@@ -128,15 +128,71 @@ def EFSW_P1():
                 runner = SingleTestRunner("EFSW", buffer_size, trace_file, [half_life_ratio, 1, 1])
                 hit_rate = runner.get_hit_rate()
                 hit_rate_list.append(hit_rate)
-            ax.plot(param_list, hit_rate_list, label=f'{"EFSW"}_{buffer_size}', marker='+', linestyle='dashed')
+            ax.plot(param_list, hit_rate_list, label=f'{"ESFW"}_{buffer_size}', marker='+', linestyle='dashed')
         plt.legend(loc=2)
         fig_path = f'plots/3.1.2/{trace_name}_ESFW.png'
         plt.savefig(fig_path)
         print(f'Fig generated path: {fig_path}. ')
 
+def COMBINE_OLTP():
+    fig, axes = plt.subplots(1, 2, figsize=(14, 7))
+    ax = axes[0][0]
+    for trace_name in ["OLTP"]:
+        print("TRACE NAME:", trace_name)
+        # fig, ax = plt.subplots(figsize=(14, 7))
+        ax.set_xlabel('Param (lambda)')
+        ax.set_ylabel('Hit Ratio(%)')
+        # ax.set_title(trace_name)
+        ax.set_xscale('log')
+        trace_file = f'traces/{trace_name}.lis'
+        param_list = [math.pow(10, i) for i in np.arange(-6, 0, 0.1)]
+        ax.set_xlim(param_list[0] / 2, param_list[-1] * 2)
+        # PL = [math.pow(2, i) for i in np.arange(0, 9)]
+        ax.set_xlim(param_list[0] / 2, param_list[-1] * 2)
+        x = param_list
+        for buffer_size in [1000, 2000, 5000, 10000, 15000]:
+            hit_rate_list = []
+            for half_life_ratio in param_list:
+                runner = SingleTestRunner("LRFU", buffer_size, trace_file, [half_life_ratio, 1, 1])
+                hit_rate = runner.get_hit_rate()
+                hit_rate_list.append(hit_rate)
+            ax.plot(param_list, hit_rate_list, label=f'{"LRFU"}_{buffer_size}', marker='+', linestyle='dashed')
+        # plt.legend(loc=2)
+        # fig_path = f'plots/3.1.2/{trace_name}_LRFU.png'
+        # plt.savefig(fig_path)
+        # print(f'Fig generated path: {fig_path}. ')
+
+    ax = axes[0][1]
+    for trace_name in ["OLTP"]:
+        print("TRACE NAME:", trace_name)
+        fig, ax = plt.subplots(figsize=(14, 7))
+        ax.set_xlabel('Param (lambda)')
+        ax.set_ylabel('Hit Ratio(%)')
+        # ax.set_title(trace_name)
+        ax.set_xscale('log')
+        trace_file = f'traces/{trace_name}.lis'
+        param_list = [math.pow(10, i) for i in np.arange(-2, 3, 0.1)]
+        ax.set_xlim(param_list[0] / 2, param_list[-1] * 2)
+        # PL = [math.pow(2, i) for i in np.arange(0, 9)]
+        ax.set_xlim(param_list[0] / 2, param_list[-1] * 2)
+        x = param_list
+        for buffer_size in [1000, 2000, 5000, 10000, 15000]:
+            hit_rate_list = []
+            for half_life_ratio in param_list:
+                runner = SingleTestRunner("EFSW", buffer_size, trace_file, [half_life_ratio, 1, 1])
+                hit_rate = runner.get_hit_rate()
+                hit_rate_list.append(hit_rate)
+            ax.plot(param_list, hit_rate_list, label=f'{"EFSW"}_{buffer_size}', marker='+', linestyle='dashed')
+        # plt.legend(loc=2)
+        # fig_path = f'plots/3.1.2/{trace_name}_ESFW.png'
+    fig_path =f'plots/3.1.2/OLTP_combine.png'
+    plt.savefig(fig_path)
+    print(f'Fig generated path: {fig_path}. ')
+
 if __name__ == '__main__':
     LRFU_OLTP()
     EFSW_OLTP()
 
-    LRFU_P1()
-    EFSW_P1()
+    COMBINE_OLTP()
+    # LRFU_P1()
+    # EFSW_P1()
--- plots/3.3.4/main.py
+++ plots/3.3.4/main.py
@@ -3,71 +3,85 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
-labels = ['ALRFU', 'GLRFU', 'ARC', 'LFU', 'SRRIP']
-trace_types = ['OLTP', 'OS Disk', 'ERP', 'Read Skew', 'Read Seq', 'Alter', 'YCSB', 'Overall']
-data_overall = [101.49, 97.19, 77.31, 12.07, 16.60]
+labels = ['A-LRFU', 'RGC', 'ARC', 'LFU', 'SRRIP']
+trace_types = ['OLTP', 'OS Disk', 'ERP', 'Hotpots', 'Sequential', 'H-S', 'Overall']
+# data_overall = [101.49, 97.19, 77.31, 12.07, 16.60]
 colors=['r', 'b', 'g', 'y', 'purple']
-X = np.arange(8)
+X = np.arange(7)
 
-# data = {
-#     'ALRFU': [43.50, 129.65, 99.78, 139.46, -2.60, 44.29, 0, 101.49,],
-#     'GLRFU': [38.17, 120.44, 95.02, 147.64, -11.37, 46.86, 184.30, 97.19,],
-#     'ARC': [13.02, 102.46, 87.20, 94.57, 4.10, 49.97, 24.98, 77.31,],
-#     'LFU': [-52.93, 16.21, 92.68, 83.70, -94.68, 1.83, 9.90, 12.73,],
-#     'SRRIP': [3.67, 23.80, 37.93, 10.15, 3.80, 11.56, 37.51, 16.60,],
-# }
-# data = {
-#     'ALRFU': [4.99, 9.23, 6.34, 13.89, 1.37, 11.65, 0, 9.24],
-#     'GLRFU': [2.07, 7.77, 8.23, 15.15, -1.67, 10.94, 1.06, 8.32],
-#     'ARC': [3.93, 7.30, 6.35, 9.88, 1.27, 9.99, 0.31, 7.12],
-#     'LFU': [-31.96, -9.31, 9.20, 10.13, -572.89, 1.31, -1.42, -45.31],
-#     'SRRIP': [1.27, 4.42, 6.28, 4.69, 4.45, 4.55, -0.86, 4.27],
-# }
+"""
+DS1
+Performance rate of LFU: 16.055799756916503% average higher than lru.
+Performance rate of SRRIP: 14.322847627488175% average higher than lru.
+Performance rate of ARC: 10.859251895544181% average higher than lru.
+Performance rate of A-LRFU: 14.018522372049569% average higher than lru.
+Performance rate of RGC: 15.92908455706928% average higher than lru.
+OLTP
+Performance rate of LFU: -21.78926246434558% average higher than lru.
+Performance rate of SRRIP: 1.3676911143467383% average higher than lru.
+Performance rate of ARC: 4.206246025219573% average higher than lru.
+Performance rate of A-LRFU: 5.307867936591788% average higher than lru.
+Performance rate of RGC: 2.510720676392896% average higher than lru.
+P
+Performance rate of LFU: -2.0044724578575024% average higher than lru.
+Performance rate of SRRIP: 5.152130264834779% average higher than lru.
+Performance rate of ARC: 8.373715071739325% average higher than lru.
+Performance rate of A-LRFU: 11.512516893627678% average higher than lru.
+Performance rate of RGC: 9.58536295567872% average higher than lru.
+Performance rate of OPT: 38.04929073778998% average higher than lru.
+Hotpots
+Performance rate of LFU: 11.60398679181312% average higher than lru.
+Performance rate of SRRIP: 5.06944084824271% average higher than lru.
+Performance rate of ARC: 11.284070279507452% average higher than lru.
+Performance rate of A-LRFU: 16.696653757491408% average higher than lru.
+Performance rate of RGC: 18.571407037390575% average higher than lru.
+Performance rate of OPT: 44.90767955659087% average higher than lru.
+Sequential
+Performance rate of LFU: -60.67818467442766% average higher than lru.
+Performance rate of SRRIP: 4.878089620205224% average higher than lru.
+Performance rate of ARC: 1.3904803086388238% average higher than lru.
+Performance rate of A-LRFU: 1.7048203838827227% average higher than lru.
+Performance rate of RGC: -1.2405186257641003% average higher than lru.
+Performance rate of OPT: 51.54282271252566% average higher than lru.
+H-S
+Performance rate of LFU: 1.3900574668865704% average higher than lru.
+Performance rate of SRRIP: 4.913861644660482% average higher than lru.
+Performance rate of ARC: 11.33684073555821% average higher than lru.
+Performance rate of A-LRFU: 13.72241649488386% average higher than lru.
+Performance rate of RGC: 12.669197566173102% average higher than lru.
+Performance rate of OPT: 45.553242652438286% average higher than lru.
+"""
+
 data = {
-    'ALRFU': [5.30, 11.51, 7.12, 16.69, 1.70, 13.72, None, None],
-    'GLRFU': [2.51, 9.58, 9.85, 18.57, -1.24, 12.66, 1.22, 8.19],
-    'ARC': [4.20, 8.37, 7.02, 11.28, 1.39, 11.33, 0.32, 6.39],
-    'LFU': [-21.78, -2.00, 11.27, 11.60, -60.67, 1.39, -1.34, -3.20],
-    'SRRIP': [1.36, 5.15, 7.81, 5.06, 4.87, 4.91, -0.79, 3.48],
+    'A-LRFU': [5.30, 11.51, 7.12, 16.69, 1.70, 13.72, 11.75],
+    'RGC': [2.51, 9.58, 9.85, 18.57, -1.24, 12.66, 10.77],
+    'ARC': [4.20, 8.37, 7.02, 11.28, 1.39, 11.33, 8.53],
+    'LFU': [-21.78, -2.00, 11.27, 11.60, -60.67, 1.39, -3.29],
+    'SRRIP': [1.36, 5.15, 7.81, 5.06, 4.87, 4.91, 5.27],
 }
-# data_overall = {
-#     'ALRFU': 101.49,
-#     'GLRFU': 97.19,
-#     'ARC': 77.31,
-#     'LFU': 12.07,
-#     'SRRIP': 16.60,
-# }
 
-plt.style.use('seaborn')
+plt.style.use('seaborn-v0_8-paper')
+
+hatches = ['//', '\\', '||', '-', '+',]
 
 if __name__ == "__main__":
-    # fig = plt.figure()
-    # ax = fig.add_axes([0, 0, 1, 1])
     fig, ax = plt.subplots(figsize=(14, 7))
-    # axes:matplotlib.axes.Axes = fig.gca()
-    # for i, key in enumerate(data_overall.keys()):
-    #     ax.bar(l)
     ax.set_xticks(X + 0.4)
     ax.set_xticklabels(trace_types, fontsize=14)
-    # ax.xaxis.set_major_locator(ticker.FixedLocator(X + 0.4))
-    # ax.xaxis.set_major_formatter(ticker.FixedFormatter(trace_types))
     for i, policy in enumerate(data.keys()):
-        hatch = '//' if policy == 'ALRFU' else None
-        color = 'white' if policy == 'ALRFU' else colors[i]
+        hatch = hatches[i]
+        color = colors[i]
         x = []
         y = []
         for idx, d in enumerate(data[policy]):
             if d is None:
                 continue
-            if (idx >= 6):
-                x.append(idx + (i) / 6)
-            else:
-                x.append(idx + (i + 0.5) / 6)
+            x.append(idx + (i + 0.5) / 6)
             y.append(d)
-        ax.bar(x, y, color=color, width=1/6,
-               label=policy, bottom=0, hatch=hatch, edgecolor=colors[i],)
-    ax.set_title('Comparing the Performance of Various Policies and LRU', fontsize=14)
-    ax.set_ylabel('Performance higher than LRU (%)', fontsize=14)
+        ax.bar(x, y, color='white', width=1/6,
+               label=policy, bottom=0, hatch=hatch, edgecolor=color, linewidth = 1)
+    # ax.set_title('Comparing the Performance of Various Policies and LRU', fontsize=14)
+    ax.set_ylabel('Miss ratio lower than LRU (%)', fontsize=14)
     ax.set_ylim(-4.99, 19.99)
     plt.legend()
     plt.savefig('plots/3.3.4/1.png')
